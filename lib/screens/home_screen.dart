48d35cd63fd5f4eb1f9cba58649e9c1f290efda7
luiscg01 <luiscg0101@gmail.com>
2023-02-13 01:44:22 -0800

    Detalles HomeScreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,15 +1,26 @@
 import  'package:flutter/material.dart';
+import 'package:peliculas/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   
 
   Widget build (BuildContext context) {
    return Scaffold(
-    body:  return Container(
-      child: Center(
-        child: Text('Home Screen'),
-      ),
+    appBar: AppBar(
+      title: Text('Peliculas de cine'),
+      elevation: 0,
+      actions: [
+        IconButton(
+          icon: Icon(Icons.search_outlined),
+          onPressed: (){},
+        ),
+      ]
     ),
+    body: Column(
+      children: [
+
+      ],
+    )
    );
   }
 }
