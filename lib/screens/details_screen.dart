62914433abf806e06d29da8569e2cda9903a5fe2
luiscg01 <luiscg0101@gmail.com>
2023-02-13 01:24:23 -0800

    Division de rutas

---
+++ lib/screens/details_screen.dart
@@ -0,0 +1,15 @@
+import  'package:flutter/material.dart';
+
+class DetailsScreen extends StatelessWidget {
+  
+
+  Widget build (BuildContext context) {
+   return Scaffold(
+    body:  return Container(
+      child: Center(
+        child: Text('Details Screen'),
+      ),
+    ),
+   );
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,15 @@
+import  'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  
+
+  Widget build (BuildContext context) {
+   return Scaffold(
+    body:  return Container(
+      child: Center(
+        child: Text('Home Screen'),
+      ),
+    ),
+   );
+  }
+}
